6d80fd1a6be4111482f0c9ca292aa2866fb95607
McLovinCSUN <60550200+McLovinCSUN@users.noreply.github.com>
2020-08-20 20:26:56 -0700

    Update
    added the forecast on the bottom and current location button to function

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 function formatDate(timestamp) {
   let date = new Date(timestamp);
 
-  let minutes = date.getMinutes();
-  if (minutes < 10) {
-    minutes = `0${minutes}`;
-  }
   let days = [
     "Sunday",
     "Monday",
@@ -14,63 +10,25 @@ function formatDate(timestamp) {
     "Saturday",
     "Sunday",
   ];
-  let time = [
-    "12",
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10",
-    "11",
-    "12",
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10",
-    "11",
-  ];
+
   let day = days[date.getDay()];
-  let hours = time[date.getHours()];
-  if (hours < 10) {
-    hours = `0${hours}`;
-  }
-  let displayDate;
-  if (date.getHours() <= 11 || date.getHours() === 24) {
-    displayDate = `${day} ${hours}:${minutes} AM`;
-  } else {
-    displayDate = `${day} ${hours}:${minutes} PM`;
-  }
-  return displayDate;
+
+  return `${day} ${formatHours(timestamp)}`;
 }
 
 function getWeather(response) {
-  console.log(response.data);
   let tempF = document.querySelector("#temp");
   let currentCity = document.querySelector("#currentCity");
   let descriptionElement = document.querySelector("#description");
   let humidity = document.querySelector("#humidity");
   let wind = document.querySelector("#wind");
-
-  // let tempC = document.querySelector("#temp");
   let dateElement = document.querySelector("#date");
   let country = document.querySelector("#country");
   let city = response.data.name;
   let temp = response.data.main.temp;
   let icon = document.querySelector("#icon");
-  fernTemp = response.data.main.temp;
 
-  // tempC.addEventListener("click", changeTemp);
+  fernTemp = response.data.main.temp;
   currentCity.innerHTML = city;
   tempF.innerHTML = Math.round(fernTemp);
   humidity.innerHTML = response.data.main.humidity;
@@ -103,12 +61,79 @@ function fernTempConversion(event) {
   tempF.innerHTML = Math.round(fernTemp);
 }
 
+function formatHours(timestamp) {
+  let date = new Date(timestamp);
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let time = [
+    "12",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "11",
+    "12",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "11",
+  ];
+  let hours = time[date.getHours()];
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  if (date.getHours() <= 11 || date.getHours() === 24) {
+    return `${hours}:${minutes} AM`;
+  } else {
+    return `${hours}:${minutes} PM`;
+  }
+}
+
+function displayForecast(response) {
+  console.log(response.data);
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = null;
+  let forecast = null;
+
+  for (let i = 0; i < 6; i++) {
+    forecast = response.data.list[i];
+    forecastElement.innerHTML += `<div class="col"><div class="col-2">
+    <h4>${formatHours(forecast.dt * 1000)}</h4>
+    <img src="http://openweathermap.org/img/wn/${
+      response.data.list[i].weather[0].icon
+    }@2x.png" alt="" id="icon" />
+      <div class="weather-forecast-temperature">
+      <strong>${Math.round(forecast.main.temp_max)}°</strong> | ${Math.round(
+      forecast.main.temp_min
+    )}°
+    </div></div></div>`;
+  }
+}
 function search(city) {
   let apiKey = "4c362e7f4f84ef4ab0ee164594102485";
   let apiUrl = "https://api.openweathermap.org/data/2.5/weather?";
   axios
     .get(`${apiUrl}q=${city}&appid=${apiKey}&units=imperial`)
     .then(getWeather);
+
+  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function handleSubmit(event) {
@@ -116,19 +141,6 @@ function handleSubmit(event) {
   let searchInput = document.querySelector("#form");
   search(searchInput.value);
 }
-// let updateCity = document.querySelector("#currentCity");
-// let latitude;
-// let longitude;
-
-// function handlePosition(response) {
-//   latitude = response.coords.latitude;
-//   longitude = response.coords.longitude;
-//   axios
-//     .get(
-//       `${apiUrl}lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`
-//     )
-//     .then(getWeather);
-// }
 
 search("Los Angeles");
 let fernTemp = null;
@@ -142,25 +154,22 @@ celLink.addEventListener("click", celTempConversion);
 let fernLink = document.querySelector("#fernLink");
 fernLink.addEventListener("click", fernTempConversion);
 
-// function searchWeather(response) {
-//   axios
-//     .get(`${apiUrl}q=${response}&appid=${apiKey}&units=imperial`)
-//     .then(getSearchWeather);
-// }
+function click() {
+  navigator.geolocation.getCurrentPosition(handlePosition);
+}
+function handlePosition(response) {
+  let apiKey = "4c362e7f4f84ef4ab0ee164594102485";
+  let apiUrl = "https://api.openweathermap.org/data/2.5/weather?";
+  let latitude = response.coords.latitude;
+  longitude = response.coords.longitude;
+  axios
+    .get(
+      `${apiUrl}lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`
+    )
+    .then(getWeather);
+}
 
-// function getSearchWeather(response) {
-//   let tempF = document.querySelector("#temp");
-//   let temp = response.data.main.temp;
-//   tempF.innerHTML = Math.round(temp);
-// }
-
-// function changeTemp(event) {
-//   event.preventDefault();
-//   tempC.innerHTML = "28.3";
-// }
-// function click() {
-//   navigator.geolocation.getCurrentPosition(handlePosition);
-// }
-// let currentLocation = document.querySelector("#currentLocation");
-// currentLocation.addEventListener("click", click);
-// window.addEventListener("load", getWeather);
+let latitude;
+let longitude;
+let currentLocation = document.querySelector("#currentLocation");
+currentLocation.addEventListener("click", click);
